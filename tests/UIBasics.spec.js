5837bdf0dd53a91a1d77bbf5908778a26eaeb4fc
Ankit Sapara <saparaankit@gmail.com>
2026-05-25 19:50:03 +0530

    Initial commit

---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,32 @@
+const {test} = require('@playwright/test');
+
+
+
+test('First Case Type 1 with function', async function()
+{
+});
+
+
+test('launch browser context declaration for in incognito mode', async ({browser})=>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://www.google.com/'); 
+});
+
+test('Launch Browser in normal Way with  cookies', async ({browser, page})=>
+{
+    // const context = await browser.newContext();
+    // const page = await context.newPage();
+    await page.goto('https://www.google.com/');
+});
+
+
+
+
+test('launch browser in default mode', async ({page})=>
+{
+
+    await page.goto('https://www.google.com/'); 
+});
+
