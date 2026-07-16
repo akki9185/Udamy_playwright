8acad3d9eea845bfb3622107161e29f42345308b
Ankit Sapara <saparaankit@gmail.com>
2026-07-16 17:19:01 +0530

    Add Practice cases

--- tests/product_page.spec.js
+++ tests/product_page.spec.js
@@ -6,9 +6,8 @@ test("Get all product name", async ({ page }) => {
     await page.locator("#username").fill("rahulshettyacademy");
     await page.locator("#password").fill("Learning@830$3mK2");
     await page.locator("#login").click();
+    // await page.waitForLoadState("networkidle"); this is Fleky approch, sometimes it is work and sometimes its not so we use below.
     await page.locator(".card-body h1 a").last().waitFor();
-    // await page.waitForLoadState("networkidle");
     console.log(await page.locator(".card-body h1 a").allTextContents());
-
 
 });
